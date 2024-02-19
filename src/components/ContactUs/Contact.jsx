6fa254ee60c689bfa222fec1c6d8fcfc0b4a5e07
Muhammad Rizki Budiman <rizkiboediman59@gmail.com>
2024-02-19 11:30:15 +0700

    Validasi Contact dan Hubungkan ke Whatsapp (Belum Email)

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 export const Contact = () => {
-  //   const style ={
+  const [validated, setValidated] = useState(false);
+  const [showEmail, setShowEmail] = useState(true);
+  const [showPhone, setShowPhone] = useState(false);
+  const [send, setSend] = useState('1');
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    phone: null,
+    asal: '',
+    message: '',
+  });
 
-  //   }
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      const phone = '+628562593941';
+      const message = `Saya ${data.name} berasal dari ${data.asal}. ${data.message} `;
+      const whatsappURL = `https://wa.me/${phone}?text=${encodeURIComponent(
+        message
+      )}`;
+      window.open(whatsappURL, '_blank');
+    }
+
+    setValidated(true);
+  };
+
+  const handleSelect = (e) => {
+    const value = e.target.value;
+    value === '1'
+      ? (setShowEmail(true), setShowPhone(false), setSend('1'))
+      : (setShowPhone(true), setShowEmail(false), setSend('2'));
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <>
       <Container className="px-lg-4 py-lg-5 px-md-3 py-md-3 px-4 py-5">
@@ -13,55 +53,139 @@ export const Contact = () => {
         </div>
         <hr />
         <div>
-          <form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <div className="mb-3">
-              <label htmlFor="nama" className="form-label">
-                Nama Kamu
-              </label>
-              <input type="text" className="form-control" id="nama" />
+              <Form.Group>
+                <Form.Label className="form-label">Nama Kamu</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Mohon isi nama kamu
+                </Form.Control.Feedback>
+              </Form.Group>
             </div>
             <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input type="email" className="form-control" id="email" />
+              <Form.Group>
+                <Form.Label className="form-label">Send Via</Form.Label>
+                <Form.Select
+                  size="sm"
+                  className="form-control"
+                  onChange={(e) => handleSelect(e)}
+                >
+                  <option value="1">Email</option>
+                  <option value="2">Whatsapp</option>
+                </Form.Select>
+              </Form.Group>
             </div>
-            <div className="d-flex ">
-              <div
-                className="mb-3"
-                style={{ width: '50%', marginRight: '10px' }}
-              >
-                <label htmlFor="whatsApp" className="form-label">
-                  WhatsApp
-                </label>
-                <input type="number" className="form-control" id="whatsApp" />
+            {showEmail && (
+              <div className="d-flex ">
+                <div
+                  className="mb-3 fade-in"
+                  style={{ width: '50%', marginRight: '10px' }}
+                >
+                  <Form.Group>
+                    <Form.Label className="form-label">Email</Form.Label>
+                    <Form.Control
+                      required
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi email kamu
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+                <div className="mb-3 fade-in" style={{ width: '50%' }}>
+                  <Form.Group>
+                    <Form.Label className="form-label">Asal</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      className="form-control"
+                      id="asal"
+                      name="asal"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi asal kamu
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div>
               </div>
-              <div className="mb-3" style={{ width: '50%' }}>
-                <label htmlFor="asal" className="form-label">
-                  Asal
-                </label>
-                <input type="text" className="form-control" id="asal" />
-              </div>
-            </div>
+            )}
+            {showPhone && (
+              <>
+                {/* <div className="mb-3 fade-in">
+                  <Form.Group>
+                    <Form.Label className="form-label">WhatsApp</Form.Label>
+                    <Form.Control
+                      required
+                      type="number"
+                      className="form-control"
+                      id="phone"
+                      name="phone"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi nomor whatsapp kamu
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div> */}
+                <div className="mb-3 fade-in">
+                  <Form.Group>
+                    <Form.Label className="form-label">Asal</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      className="form-control"
+                      id="asal"
+                      name="asal"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi asal kamu
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </div>
+              </>
+            )}
             <div className="mb-3">
-              <label htmlFor="pesan" className="fs-5 fw-semibold">
-                Pesanmu
-              </label>
-              <textarea
-                className="form-control"
-                placeholder="Assalamualaikum"
-                id="pesan"
-                style={{ resize: 'none', height: '10rem' }}
-              ></textarea>
+              <Form.Group>
+                <Form.Label htmlFor="pesan" className="fs-5 fw-semibold">
+                  Pesanmu
+                </Form.Label>
+                <Form.Control
+                  required
+                  as="textarea"
+                  className="form-control"
+                  placeholder="Assalamualaikum"
+                  id="message"
+                  name="message"
+                  onChange={handleChange}
+                  style={{ resize: 'none', height: '10rem' }}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Mohon isi pesan yang ingin disampaikan
+                </Form.Control.Feedback>
+              </Form.Group>
             </div>
-            <button
+            <Button
               type="submit"
               className="btn btn-primary fw-semibold"
               style={{ borderRadius: '50px', padding: '3px 20px' }}
             >
               Send
-            </button>
-          </form>
+            </Button>
+          </Form>
         </div>
       </Container>
     </>
--- src/components/Detail/DetailVilla.jsx
+++ src/components/Detail/DetailVilla.jsx
@@ -10,10 +10,13 @@ import { Faq } from '../Faq/Faq';
 import { Contact } from '../ContactUs/Contact';
 import { Footer } from '../Footer/Footer';
 import { MobileTestimonials } from '../Testimonials/MobileTestimonials';
+import { useMediaQuery } from 'react-responsive';
 
 export const DetailVilla = () => {
   const { id } = useParams();
   console.log('ini id nya', id);
+
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
   const villa = dataVilla.find((item) => item.id == id);
   return (
@@ -21,7 +24,7 @@ export const DetailVilla = () => {
       <Container>
         <DetailVillaUp dataVilla={villa} />
         <DetailVillaDown dataVilla={villa} />
-        <MobileTestimonials />
+        {isMobile && <MobileTestimonials />}
         <VillaRecomendation />
         <UserTestimonials />
         <Faq />
