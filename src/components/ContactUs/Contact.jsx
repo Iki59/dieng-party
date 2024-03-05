8d7f941b82707784bc189f3d047486d187022b7f
Muhammad Rizki Budiman <rizkiboediman59@gmail.com>
2024-03-05 09:22:21 +0700

    resolve conflict

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
-import emailjs from "@emailjs/browser";
-import { errorNotification, successNotification } from "../../utils/Notifications";
+import React, { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
+import {
+  errorNotification,
+  successNotification,
+} from '../../utils/Notifications';
 
 export const Contact = () => {
   const [validated, setValidated] = useState(false);
   const [showEmail, setShowEmail] = useState(true);
   const [showPhone, setShowPhone] = useState(false);
-  const [send, setSend] = useState("1");
+  const [send, setSend] = useState('1');
   const [data, setData] = useState({
-    name: "",
-    email: "",
+    name: '',
+    email: '',
     phone: null,
-    asal: "",
-    message: "",
+    asal: '',
+    message: '',
   });
 
   const handleSubmit = (event) => {
@@ -22,9 +25,8 @@ export const Contact = () => {
       event.preventDefault();
       event.stopPropagation();
     } else {
-      if (send === "1") {
+      if (send === '1') {
         event.preventDefault();
-<<<<<<< HEAD
         try {
           emailjs.sendForm('service_6852cjr', 'template_0uwexia', form, {
             publicKey: 'uMJ9DGnQynwlhNM0J',
@@ -34,26 +36,13 @@ export const Contact = () => {
           errorNotification('Email');
           console.log(error);
         }
-=======
-        emailjs
-          .sendForm("service_6852cjr", "template_0uwexia", form, {
-            publicKey: "uMJ9DGnQynwlhNM0J",
-          })
-          .then(
-            () => {
-              successNotification("Email");
-            },
-            (error) => {
-              errorNotification("Email");
-              console.log(error);
-            }
-          );
->>>>>>> 50b68a6484a47b7219b42846c068be26c41bdac4
       } else {
-        const phone = "+628562593941";
+        const phone = '+628562593941';
         const message = `Saya ${data.name} berasal dari ${data.asal}. ${data.message} `;
-        const whatsappURL = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-        window.open(whatsappURL, "_blank");
+        const whatsappURL = `https://wa.me/${phone}?text=${encodeURIComponent(
+          message
+        )}`;
+        window.open(whatsappURL, '_blank');
       }
     }
 
@@ -62,7 +51,9 @@ export const Contact = () => {
 
   const handleSelect = (e) => {
     const value = e.target.value;
-    value === "1" ? (setShowEmail(true), setShowPhone(false), setSend("1")) : (setShowPhone(true), setShowEmail(false), setSend("2"));
+    value === '1'
+      ? (setShowEmail(true), setShowPhone(false), setSend('1'))
+      : (setShowPhone(true), setShowEmail(false), setSend('2'));
   };
 
   const handleChange = (e) => {
@@ -84,14 +75,27 @@ export const Contact = () => {
             <div className="mb-3">
               <Form.Group>
                 <Form.Label className="form-label">Nama Kamu</Form.Label>
-                <Form.Control required type="text" className="form-control" id="name" name="name" onChange={handleChange} />
-                <Form.Control.Feedback type="invalid">Mohon isi nama kamu</Form.Control.Feedback>
+                <Form.Control
+                  required
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Mohon isi nama kamu
+                </Form.Control.Feedback>
               </Form.Group>
             </div>
             <div className="mb-3">
               <Form.Group>
                 <Form.Label className="form-label">Send Via</Form.Label>
-                <Form.Select size="sm" className="form-control" onChange={(e) => handleSelect(e)}>
+                <Form.Select
+                  size="sm"
+                  className="form-control"
+                  onChange={(e) => handleSelect(e)}
+                >
                   <option value="1">Email</option>
                   <option value="2">Whatsapp</option>
                 </Form.Select>
@@ -99,18 +103,39 @@ export const Contact = () => {
             </div>
             {showEmail && (
               <div className="d-flex ">
-                <div className="mb-3 fade-in" style={{ width: "50%", marginRight: "10px" }}>
+                <div
+                  className="mb-3 fade-in"
+                  style={{ width: '50%', marginRight: '10px' }}
+                >
                   <Form.Group>
                     <Form.Label className="form-label">Email</Form.Label>
-                    <Form.Control required type="email" className="form-control" id="email" name="email" onChange={handleChange} />
-                    <Form.Control.Feedback type="invalid">Mohon isi email kamu</Form.Control.Feedback>
+                    <Form.Control
+                      required
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi email kamu
+                    </Form.Control.Feedback>
                   </Form.Group>
                 </div>
-                <div className="mb-3 fade-in" style={{ width: "50%" }}>
+                <div className="mb-3 fade-in" style={{ width: '50%' }}>
                   <Form.Group>
                     <Form.Label className="form-label">Asal</Form.Label>
-                    <Form.Control required type="text" className="form-control" id="asal" name="asal" onChange={handleChange} />
-                    <Form.Control.Feedback type="invalid">Mohon isi asal kamu</Form.Control.Feedback>
+                    <Form.Control
+                      required
+                      type="text"
+                      className="form-control"
+                      id="asal"
+                      name="asal"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi asal kamu
+                    </Form.Control.Feedback>
                   </Form.Group>
                 </div>
               </div>
@@ -136,18 +161,24 @@ export const Contact = () => {
                 <div className="mb-3 fade-in">
                   <Form.Group>
                     <Form.Label className="form-label">Asal</Form.Label>
-                    <Form.Control required type="text" className="form-control" id="asal" name="asal" onChange={handleChange} />
-                    <Form.Control.Feedback type="invalid">Mohon isi asal kamu</Form.Control.Feedback>
+                    <Form.Control
+                      required
+                      type="text"
+                      className="form-control"
+                      id="asal"
+                      name="asal"
+                      onChange={handleChange}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Mohon isi asal kamu
+                    </Form.Control.Feedback>
                   </Form.Group>
                 </div>
               </>
             )}
             <div className="mb-3">
               <Form.Group>
-                <Form.Label htmlFor="pesan" className="fs-5 fw-semibold">
-                  Pesanmu
-                </Form.Label>
-<<<<<<< HEAD
+                <Form.Label className="fs-5 fw-semibold">Pesanmu</Form.Label>
                 <Form.Control
                   required
                   as="textarea"
@@ -161,13 +192,13 @@ export const Contact = () => {
                 <Form.Control.Feedback type="invalid">
                   Mohon isi pesan kamu
                 </Form.Control.Feedback>
-=======
-                <Form.Control required as="textarea" className="form-control" placeholder="Assalamualaikum" id="message" name="message" onChange={handleChange} style={{ resize: "none", height: "10rem" }} />
-                <Form.Control.Feedback type="invalid">Mohon isi pesan yang ingin disampaikan</Form.Control.Feedback>
->>>>>>> 50b68a6484a47b7219b42846c068be26c41bdac4
               </Form.Group>
             </div>
-            <Button type="submit" className="btn btn-primary fw-semibold" style={{ borderRadius: "50px", padding: "3px 20px" }}>
+            <Button
+              type="submit"
+              className="btn btn-primary fw-semibold"
+              style={{ borderRadius: '50px', padding: '3px 20px' }}
+            >
               Send
             </Button>
           </Form>
