e9d0771baeb922046b44038542bdb86b60f3ec78
rojiloro <kelengoji79@gmail.com>
2024-08-10 11:15:07 +0700

    icon done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "../../assets/RY2.png";
 import { Col, Row } from "react-bootstrap";
+import LogoInstagram from "../../assets/instagram.png";
+import LogoTiktok from "../../assets/tiktok.png";
 
 export const Footer = () => {
   return (
@@ -28,26 +30,23 @@ export const Footer = () => {
             <Col md={3} className="mb-4">
               <img src={Logo} style={{ width: "12rem" }} />
             </Col>
-            <Col md={2} className="d-sm-block d-flex">
-              <p className="me-5" style={{ marginBottom: "2rem" }}>
-                {" "}
+            <Col md={4} className="d-sm-block d-flex mt-5">
+              <p className="me-5" style={{ marginBottom: "2rem", fontWeight: "bold" }}>
+                <p>Alamat: Jl. Dieng, Batur, Banjarnegara, Jawa Tengah</p>
               </p>
               <p> </p>
             </Col>
-            <Col md={2} className="d-sm-block d-flex">
-              <p className="me-5" style={{ marginBottom: "2rem" }}>
-                {" "}
-              </p>
-              <p> </p>
-            </Col>
-            <Col md={2} className="d-sm-block d-flex">
-              <p className="me-5" style={{ marginBottom: "2rem" }}>
-                Villa
-              </p>
-              <p>Paket Wisata</p>
+            <Col md={2} className="d-sm-block d-flex mt-5">
+              <p>Telepon: +6281237339956</p>
             </Col>
             <Col md={3}>
-              <p className="fw-bold">Join our community 🔥</p>
+              <p className="fw-bold mt-5">check our social media 🔥</p>
+              <a href="https://www.instagram.com/houseofdieng/" target="_blank">
+                <img src={LogoInstagram} alt="instagram" style={{ width: "2rem" }} />
+              </a>
+              <a href="https://www.tiktok.com/@house_of_dieng" target="_blank">
+                <img src={LogoTiktok} alt="tiktok" style={{ width: "5.8rem" }} />
+              </a>
             </Col>
             <div className="mt-4">
               <p className="fs-10">Copyright © 2023 Techmas Dev. All rights reserved</p>
